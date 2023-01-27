22199a831c74ef78c1201c429bbdd0efd9c0e365
TheSavageTeddy <51810476+TheSavageTeddy@users.noreply.github.com>
2023-01-27 17:53:35 +0800

    added number and string encoding

--- obfuscators/encoders.py
+++ obfuscators/encoders.py
@@ -1,10 +1,23 @@
+class StringManipulators:
+    def decimal_to_chrs(self, array: list) -> str:
+        '''
+        Wraps each element of array in `chr()` and concatenates them.
 
+        Example: `[101, 102]` -> `chr(101)+chr(102)`
+        '''
+        return f'chr({")+chr(".join(map(str, array))})'
 
-def string_to_chrs(text: str):
-    '''
-    Simply converts strings to their decimal equivalent, wrapped in chr() and concatenated.
-    
-    Example: "Hello" -> "chr(72)+chr(101)+chr(108)+chr(108)+chr(111)"
-    '''
-    return f'chr({")+chr(".join(map(str, map(ord, text)))})' # sorry for unreadable code
+    def string_to_chrs(self, text: str) -> str:
+        '''
+        Converts strings to their decimal equivalent, wrapped in `chr()` and concatenated.
+
+        Example: `Hello` -> `chr(72)+chr(101)+chr(108)+chr(108)+chr(111)`
+        '''
+        return self.decimal_to_chrs(list(map(ord, text)))
+
+class NumberManipulators:
+    def int_addition(self, num: int) -> str:
+        '''
+        '''
+        return "+".join(["1"] * num)
 
